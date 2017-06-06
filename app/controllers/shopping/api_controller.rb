8697478939791d63b0f7deab2c23f0e82eb76ebb
Erik Sälgström Peterson <erik@barkbox.com>
2017-06-06 13:44:23 -0400

    Fix

--- app/controllers/shopping/api_controller.rb
+++ app/controllers/shopping/api_controller.rb
@@ -2,9 +2,9 @@ module Shopping
   class ApiController < ApplicationController
     include JSONAPI::ActsAsResourceController
     protect_from_forgery with: :null_session
+  end
 
     def context
       { resource_owner: current_user }
     end
-  end
 end
