a2dff53f2fafc7ce8f088818ccc33ef4287762ca
Erik Sälgström Peterson <erik@barkbox.com>
2017-07-07 13:01:58 -0400

    Add meta to argument sig

--- app/services/shopping/update_line_item_service.rb
+++ app/services/shopping/update_line_item_service.rb
@@ -8,7 +8,7 @@ module Shopping
     validates :cart, :source, presence: true
     validate :unpurchased_cart
 
-    def initialize cart, source, quantity=1
+    def initialize cart, source, quantity=1, meta={}
       @cart = cart
       @source = source
       @quantity = quantity
