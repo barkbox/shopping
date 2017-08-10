14780e091f6a35eb75a86a1a2e6111f998ba403a
Dan Hunter <dan@thisishunter.com>
2017-08-10 11:43:08 -0400

    cannot modify line items of purchased cart

--- app/models/shopping/line_item.rb
+++ app/models/shopping/line_item.rb
@@ -4,6 +4,7 @@ module Shopping
     belongs_to :source, polymorphic: true
     
     validate :unique_source_and_cart, on: :create
+    validate :unpurchased_cart
     
     validates :quantity, allow_nil: true, numericality: {only_integer: true, greater_than: -1}
     validates :cart_id, presence: true
@@ -16,6 +17,14 @@ module Shopping
       other = Shopping::LineItem.where(source_id: self.source_id, source_type: self.source_type, cart_id: self.cart_id)
       if other.count > 0
         self.errors.add(:source, "A line item with identical source and cart exists, update quantity instead (id=#{other.first.id})")
+      end
+    end
+
+    def unpurchased_cart
+      if cart && cart.purchased?
+        changes.keys.each do |k|
+          self.errors.add(k, "Cannot change `#{k}', cart is purchased")
+        end
       end
     end
 
