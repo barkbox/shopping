9f230a81e5e62bfcc434c862b8be3198090093d6
Erik Sälgström Peterson <erik@barkbox.com>
2017-09-25 14:26:25 -0400

    Add meta to cart resource

--- app/resources/shopping/cart_resource.rb
+++ app/resources/shopping/cart_resource.rb
@@ -2,16 +2,16 @@ module Shopping
   class CartResource < JSONAPI::Resource
     model_name 'Shopping::Cart'
     
-    attributes :user_id, :purchased_at, :created_at, :updated_at, :origin
+    attributes :user_id, :purchased_at, :created_at, :updated_at, :origin, :meta
     has_many :line_items
     has_many :cart_purchases
 
     def self.updatable_fields(context)
-      super - [:user_id, :updated_at, :created_at, :purchased_at, :origin]
+      super - [:user_id, :updated_at, :created_at, :purchased_at, :origin, :meta]
     end
 
     def self.creatable_fields(context)
-      super - [:updated_at, :created_at, :purchased_at]
+      super - [:updated_at, :created_at, :purchased_at, :meta]
     end
     
   end
