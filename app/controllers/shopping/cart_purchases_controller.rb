52a6a9ce079a67f5d7374dcbbdb0c91a5e4b5139
thwang <tim@barkbox.com>
2017-10-04 15:35:38 -0400

    remove comments in carts_purchases_controller

--- app/controllers/shopping/cart_purchases_controller.rb
+++ app/controllers/shopping/cart_purchases_controller.rb
@@ -1,29 +1,4 @@
 module Shopping
   class CartPurchasesController < ApiController
-
-# DONT DO THIS
-# lean on the resource and not controller
-    # def show
-    #   cart_purchase = CartPurchase.find(params[:id])
-    #   binding.pry
-    #   if @current_user.id == cart_purchase.cart.user_id
-    #     super
-    #   else
-    #     raise "FAIL: #{__FILE__} #{__METHOD__}"
-    #   end
-    # end
-
-# DONT DO THIS
-# lean on the resource and not controller
-    # def update
-      # need to verify that user is
-      #   logged in
-      #   cart owner
-
-      # # params = params[:data][:attributes]
-      # cart = Cart.where(user_id: params[:user_id], id: params[:id]).first
-      # cart.update_attributes!(params[:attributes])
-      # super
-    # end
   end
 end
